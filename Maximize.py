f9f151780c297faeea2d9c51ffbae9dc63185bf4
Anurag Mishra <mishraanurag218@gmail.com>
2021-05-31 15:57:26 +0530

    Version 1

--- Maximize.py
+++ Maximize.py
@@ -6,9 +6,7 @@ N = [[2, 5, 4], [3, 7, 8, 9], [5, 5, 7, 8, 9, 10]]
 from itertools import product
 
 possible_combination = set(list(product(*N)))
-
 print(possible_combination)
-
 def func(nums):
     return sum(x * x for x in nums) % M
 
